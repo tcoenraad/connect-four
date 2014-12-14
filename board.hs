7a298e141e7ae45925f3bfc2365300ea20600db9
Twan Coenraad <twan@coenraad.at>
2014-12-13 22:33:00 -0500

    Detect winning moves in horizontal and vertical directions

--- board.hs
+++ board.hs
@@ -10,16 +10,36 @@ module Board where
   columns :: Int
   columns = 7
 
+  strike :: Int
+  strike = 4
+
   index :: Board -> (Int, Int) -> Maybe Coin
-  index cs (x, y) | x < 0 || x >= columns = Nothing
+  index b (x, y) | x < 0 || x >= columns = Nothing
                   | y < 0 || y >= rows = Nothing
                   | length column <= y = Just Empty
                   | otherwise = Just $ column !! y
                   where
-                    column = (cs !! x)
+                    column = (b !! x)
 
   dropCoin :: Board -> Coin -> Int -> Maybe Board
-  dropCoin cs coin i | length column >= rows = Nothing
-                     | otherwise = Just $ (take i cs) ++ [(column ++ [coin])] ++ (drop (i + 1) cs)
+  dropCoin b coin i | length column >= rows = Nothing
+                     | otherwise = Just $ (take i b) ++ [(column ++ [coin])] ++ (drop (i + 1) b)
                      where
-                       column = (cs !! i)
+                       column = (b !! i)
+
+  winningMove :: Board -> (Int, Int) -> Bool
+  winningMove b (x, y) = left + right >= strike
+                        || up + down >= strike
+    where
+      left = consecutive_coins_length b (x, y) (-1, 0) 0
+      right = consecutive_coins_length b (x, y) (1, 0) 0
+      up = consecutive_coins_length b (x, y) (0, -1) 0
+      down = consecutive_coins_length b (x, y) (0, 1) 0
+
+  consecutive_coins_length :: Board -> (Int, Int) -> (Int, Int) -> Int -> Int
+  consecutive_coins_length b (x, y) (diff_x, diff_y) shift | shift == strike = 0
+                                                           | coin == coin' = 1 + consecutive_coins_length b (x, y) (diff_x, diff_y) (shift + 1)
+                                                           | otherwise = 0
+    where
+      coin = index b (x, y)
+      coin' = index b (x + diff_x * shift, y + diff_y * shift)
--- test/board.hs
+++ test/board.hs
@@ -34,3 +34,27 @@ main = hspec $ do
       index [[],[],[],[],[],[],[]] (0, -1) `shouldBe` Nothing
       index [[],[],[],[],[],[],[]] (7, 5) `shouldBe` Nothing
       index [[],[],[],[],[],[],[]] (6, 6) `shouldBe` Nothing
+
+    it "should detect a horizontal strike from the start" $ do
+      winningMove [[Yellow],[Yellow],[Yellow],[Yellow],[],[],[]] (0, 0) `shouldBe` True
+
+    it "should detect a horizontal strike halfway" $ do
+      winningMove [[Yellow],[Yellow],[Yellow],[Yellow],[],[],[]] (2, 0) `shouldBe` True
+
+    it "should detect a horizontal strike from the end" $ do
+      winningMove [[Yellow],[Yellow],[Yellow],[Yellow],[],[],[]] (3, 0) `shouldBe` True
+
+    it "should detect a not connected horizontal strike" $ do
+      winningMove [[Yellow],[Yellow],[],[Yellow],[Yellow],[],[]] (1, 0) `shouldBe` False
+
+    it "should detect a vertical strike from the start" $ do
+      winningMove [[Yellow,Yellow,Yellow,Yellow],[],[],[],[],[],[]] (0, 0) `shouldBe` True
+
+    it "should detect a vertical strike halfway" $ do
+      winningMove [[Yellow,Yellow,Yellow,Yellow],[],[],[],[],[],[]] (0, 2) `shouldBe` True
+
+    it "should detect a vertical strike from the end" $ do
+      winningMove [[Yellow,Yellow,Yellow,Yellow],[],[],[],[],[],[]] (0, 3) `shouldBe` True
+
+    it "should detect a not connected vertical strike" $ do
+      winningMove [[Yellow,Yellow,Empty,Yellow],[],[],[],[],[],[]] (0, 1) `shouldBe` False
