f095a5803080600bb9f807a0a3c327abda0233e6
Twan Coenraad <twan@coenraad.at>
2014-12-16 21:02:09 -0600

    Added strike checks on diagonals

--- board.hs
+++ board.hs
@@ -19,26 +19,32 @@ module Board where
                  | length column <= y = Just Empty
                  | otherwise = Just $ column !! y
                  where
-                   column = (b !! x)
+                   column = b !! x
 
   dropCoin :: Board -> Coin -> Int -> Maybe Board
   dropCoin b coin i | length column >= rows = Nothing
                     | otherwise = Just $ (take i b) ++ [(column ++ [coin])] ++ (drop (i + 1) b)
                     where
-                      column = (b !! i)
+                      column = b !! i
 
   winningMove :: Board -> (Int, Int) -> Bool
-  winningMove b (x, y) = left + right >= strike
-                        || down >= strike
+  winningMove b (x, y) = west + east >= strike
+                      || south >= strike
+                      || southwest + northeast >= strike
+                      || northwest + southeast >= strike
     where
-      left = consecutive_coins_length b (x, y) (-1, 0) 0
-      right = consecutive_coins_length b (x, y) (1, 0) 0
-      down = consecutive_coins_length b (x, y) (0, -1) 0
+      west = consecutive_coins_length b (x, y) (-1, 0) 0
+      east = consecutive_coins_length b (x, y) (1, 0) 0
+      south = consecutive_coins_length b (x, y) (0, -1) 0
+      southwest = consecutive_coins_length b (x, y) (-1, -1) 0
+      northeast = consecutive_coins_length b (x, y) (1, 1) 0
+      northwest = consecutive_coins_length b (x, y) (-1, 1) 0
+      southeast = consecutive_coins_length b (x, y) (1, -1) 0
 
   consecutive_coins_length :: Board -> (Int, Int) -> (Int, Int) -> Int -> Int
-  consecutive_coins_length b (x, y) (diff_x, diff_y) shift | shift == strike = 0
-                                                           | coin == coin' = 1 + consecutive_coins_length b (x, y) (diff_x, diff_y) (shift + 1)
-                                                           | otherwise = 0
+  consecutive_coins_length b (x, y) (diff_x, diff_y) offset | offset == strike = 0
+                                                            | coin == coin' = 1 + consecutive_coins_length b (x, y) (diff_x, diff_y) (offset + 1)
+                                                            | otherwise = 0
     where
       coin = index b (x, y)
-      coin' = index b (x + diff_x * shift, y + diff_y * shift)
+      coin' = index b (x + diff_x * offset, y + diff_y * offset)
--- test/board.hs
+++ test/board.hs
@@ -51,4 +51,28 @@ main = hspec $ do
       winningMove [[Yellow,Yellow,Yellow,Yellow],[],[],[],[],[],[]] (0, 3) `shouldBe` True
 
     it "should detect a not connected vertical strike" $ do
-      winningMove [[Yellow,Yellow,Empty,Yellow],[],[],[],[],[],[]] (0, 1) `shouldBe` False
+      winningMove [[Yellow,Yellow,Empty,Yellow,Yellow],[],[],[],[],[],[]] (0, 1) `shouldBe` False
+
+    it "should detect an increasing diagonal strike from the start" $ do
+      winningMove [[Yellow],[Empty,Yellow],[Empty,Empty,Yellow],[Empty,Empty,Empty,Yellow],[],[],[]] (0, 0) `shouldBe` True
+
+    it "should detect an increasing diagonal strike halfway" $ do
+      winningMove [[Yellow],[Empty,Yellow],[Empty,Empty,Yellow],[Empty,Empty,Empty,Yellow],[],[],[]] (2, 2) `shouldBe` True
+
+    it "should detect an increasing diagonal strike on the end" $ do
+      winningMove [[Yellow],[Yellow],[Yellow],[Yellow],[],[],[]] (3, 3) `shouldBe` True
+
+    it "should detect a not connected increasing diagonal strike" $ do
+      winningMove [[Yellow],[Empty,Yellow],[],[Empty,Empty,Empty,Yellow],[Empty,Empty,Empty,Empty,Yellow],[],[]] (1, 1) `shouldBe` False
+
+    it "should detect an decreasing diagonal strike from the start" $ do
+      winningMove [[Empty,Empty,Empty,Yellow],[Empty,Empty,Yellow],[Empty,Yellow],[Yellow],[],[],[]] (0, 3) `shouldBe` True
+
+    it "should detect an decreasing diagonal strike halfway" $ do
+      winningMove [[Empty,Empty,Empty,Yellow],[Empty,Empty,Yellow],[Empty,Yellow],[Yellow],[],[],[]] (2, 1) `shouldBe` True
+
+    it "should detect an decreasing diagonal strike on the end" $ do
+      winningMove [[Empty,Empty,Empty,Yellow],[Empty,Empty,Yellow],[Empty,Yellow],[Yellow],[],[],[]] (3, 0) `shouldBe` True
+
+    it "should detect a not connected decreasing diagonal strike" $ do
+      winningMove [[Empty,Empty,Empty,Empty,Yellow],[Empty,Empty,Empty,Yellow],[],[Empty,Yellow],[Yellow],[],[]] (1, 3) `shouldBe` False
