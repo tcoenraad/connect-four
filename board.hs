92d81f8390ce60cd4c40ca1893131a5cc73fc443
Twan Coenraad <twan@coenraad.at>
2014-12-30 21:31:17 +0100

    Refactored board and test

--- board.hs
+++ board.hs
@@ -33,18 +33,18 @@ module Board where
                       || southwest + northeast >= strike
                       || northwest + southeast >= strike
     where
-      west = consecutive_coins_length b (x, y) (-1, 0) 0
-      east = consecutive_coins_length b (x, y) (1, 0) 0
-      south = consecutive_coins_length b (x, y) (0, -1) 0
-      southwest = consecutive_coins_length b (x, y) (-1, -1) 0
-      northeast = consecutive_coins_length b (x, y) (1, 1) 0
-      northwest = consecutive_coins_length b (x, y) (-1, 1) 0
-      southeast = consecutive_coins_length b (x, y) (1, -1) 0
+      west = consecutiveCoinsLength b (x, y) (-1, 0) 0
+      east = consecutiveCoinsLength b (x, y) (1, 0) 0
+      south = consecutiveCoinsLength b (x, y) (0, -1) 0
+      southwest = consecutiveCoinsLength b (x, y) (-1, -1) 0
+      northeast = consecutiveCoinsLength b (x, y) (1, 1) 0
+      northwest = consecutiveCoinsLength b (x, y) (-1, 1) 0
+      southeast = consecutiveCoinsLength b (x, y) (1, -1) 0
 
-  consecutive_coins_length :: Board -> (Int, Int) -> (Int, Int) -> Int -> Int
-  consecutive_coins_length b (x, y) (diff_x, diff_y) offset | offset == strike = 0
-                                                            | coin == coin' = 1 + consecutive_coins_length b (x, y) (diff_x, diff_y) (offset + 1)
-                                                            | otherwise = 0
+  consecutiveCoinsLength :: Board -> (Int, Int) -> (Int, Int) -> Int -> Int
+  consecutiveCoinsLength b (x, y) (diff_x, diff_y) offset | offset == strike = 0
+                                                          | coin == coin' = 1 + consecutiveCoinsLength b (x, y) (diff_x, diff_y) (offset + 1)
+                                                          | otherwise = 0
     where
       coin = index b (x, y)
       coin' = index b (x + diff_x * offset, y + diff_y * offset)
