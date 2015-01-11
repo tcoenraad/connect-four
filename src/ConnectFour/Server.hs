a61386156dc6e405d79d66b2b0fe2341e074ed9f
Twan Coenraad <twan@coenraad.at>
2015-01-11 13:58:10 +0100

    Deleted queue function as largest queue is always one player

--- src/ConnectFour/Server.hs
+++ src/ConnectFour/Server.hs
@@ -2,8 +2,3 @@ module ConnectFour.Server where
 
   type Player = String
 
-  queue :: Player -> [Player] -> Int -> Either [Player] [Player]
-  queue player group i | length group' == i = Left group'
-                       | otherwise = Right group'
-                        where
-                          group' = group ++ [player]
